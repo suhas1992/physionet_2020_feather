12d1d2ea40f20d8785b5aa744180195e7d3e9df7
suhas1992 <suhas17@stanford.edu>
2020-04-24 03:59:19 -0700

    Added random forest model

--- run_12ECG_classifier.py
+++ run_12ECG_classifier.py
@@ -7,25 +7,41 @@ from get_12ECG_features import get_12ECG_features
 def run_12ECG_classifier(data,header_data,classes,model):
 
     num_classes = len(classes)
-    current_label = np.zeros(num_classes, dtype=int)
-    current_score = np.zeros(num_classes)
+    label = np.zeros(num_classes, dtype=int)
+    score = np.zeros(num_classes)
 
     # Use your classifier here to obtain a label and score for each class. 
     features=np.asarray(get_12ECG_features(data,header_data))
     feats_reshape = features.reshape(1,-1)
-    label = model.predict(feats_reshape)
-    score = model.predict_proba(feats_reshape)
 
-    current_label[label] = 1
+    prob_score = model.predict_proba(feats_reshape)
 
-    for i in range(num_classes):
-        current_score[i] = np.array(score[0][i])
+    max_prob_1 = 0
+    max_prob_index = 0
+    for i, probs in enumerate(prob_score):
+        prob_0 = probs[0][0]
+        prob_1 = probs[0][1]
+        
+        score[i] = prob_1
+        if prob_1 > 0.5:
+            label[i] = 1
+        if prob_1 > max_prob_1:
+            max_prob_1 = prob_1
+            max_prob_index = i
+    label[max_prob_index] = 1
+    # label = model.predict(feats_reshape)
+    # score = model.predict_proba(feats_reshape)
 
-    return current_label, current_score
+    # current_label[label] = 1
+
+    # for i in range(num_classes):
+    #     current_score[i] = np.array(score[0][i])
+
+    return label, score
 
 def load_12ECG_model():
     # load the model from disk 
-    filename='finalized_model.sav'
+    filename='randomforest_model.sav'
     loaded_model = joblib.load(filename)
 
     return loaded_model
