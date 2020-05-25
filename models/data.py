ce25626251aaca2984ea129eee6566f94c466bba
vedant-sanil <vsanil@andrew.cmu.edu>
2020-05-25 17:47:19 -0400

    Support val, train, test splits

--- models/data.py
+++ models/data.py
@@ -1,5 +1,6 @@
 import os 
 import pickle 
+import numpy as np
 import config as cfg 
 
 import torch
@@ -23,6 +24,16 @@ class ECGTrainSet(Dataset):
         return f, l
 
 def train_collate(batch):
+    """ Function to collate a single batch and generate padding across 
+        features along with lengths of the original features before
+        padding.
+
+        Args:
+            batch: a single batch prepared from dataloader
+
+        Returns:
+            the batch split across padded features, labels and lenghts of each feature
+    """
     features, labels = zip(*batch)
 
     features = rnn.pad_sequence(features, batch_first=True)
@@ -32,16 +43,37 @@ def train_collate(batch):
     return features, labels, lens
 
 def train_val_test_split(dataset):
-    train_size = int(0.75 * len(dataset))
-    val_size = int(0.15 * len(dataset))
-    test_size = int(0.1 * len(dataset))
+    """ Function to create samplers that will be used to
+        split the dataset given a certain random seed.
 
-    trainset, valset, testset = random_split(dataset, [train_size, val_size, test_size])
+        Args:
+            dataset: the full input dataset to be split
 
-    return trainset, valset, testset
+        Returns:
+            training data sampler, validation data sampler and test data sampler
+    """
+    dataset_size = len(dataset)
+    train_size = int(0.75 * dataset_size)
+    val_size = int(0.15 * dataset_size)
+    
+    # Create indices and shuffle them
+    indices = list(range(dataset_size))
+    np.random.seed(cfg.RANDOM_SEED)
+    np.random.shuffle(indices)
+
+    train_indices, val_indices, test_indices = indices[:train_size], indices[train_size:train_size+val_size], indices[train_size+val_size:]
+
+    # Create samplers
+    train_sampler = sampler.SubsetRandomSampler(train_indices)
+    val_sampler = sampler.SubsetRandomSampler(val_indices)
+    test_sampler = sampler.SubsetRandomSampler(test_indices)
+
+    return train_sampler, val_sampler, test_sampler
 
 # Dictionary to define Dataset stats
-config_dict = {'train':{'dataset':ECGTrainSet, 'collate':train_collate, 'shuffle':True}}
+train_sampler, val_sampler, test_sampler = train_val_test_split(ECGTrainSet)
+config_dict = {'train':{'collate':train_collate, 'shuffle':True, 'sampler':train_sampler},
+               'val':{'collate':train_collate, 'shuffle':False, 'sampler':val_sampler}}
 
 def load_pickle(path):
     """ Load data stored in pickle format
@@ -68,9 +100,10 @@ def get_loader(loader_type):
             dataloader object
     """
     feature_dict = load_pickle(cfg.OBS_DICT_PATH)
-    dataset = config_dict[loader_type]['dataset'](feature_dict)
+    dataset = ECGTrainSet(feature_dict)
 
     loader = DataLoader(dataset, 
+                        batch_sampler=config_dict[loader_type]['sampler'],
                         num_workers=cfg.NUM_WORKERS, 
                         pin_memory=cfg.PIN_MEMORY, 
                         batch_size=cfg.BATCH_SIZE,
