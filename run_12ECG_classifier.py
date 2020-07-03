0eda857697ae7d6de33278da1aa09949f2ecf872
vedant-sanil <vsanil@andrew.cmu.edu>
2020-07-03 18:36:58 -0400

    Running model integrated with driver

--- run_12ECG_classifier.py
+++ run_12ECG_classifier.py
@@ -1,11 +1,53 @@
 #!/usr/bin/env python
 
 import numpy as np
+import os
 import joblib
+import shutil 
+import tempfile 
+import torch
+from download import download_file_from_google_drive
+from models.networks.resnext import ResNet, BasicBlock
 from get_12ECG_features import get_12ECG_features
 
+# Language defined keyword
 keyword = 'pytorch'
-lang_dict = {'pytorch':{'model':load_pytorch}}
+drive = False
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def load_pytorch():
+    if drive:
+        file_id = "125FLLN21q4dvFGlIWTwUN83BdeGkE5Un"
+        tempdir = tempfile.mkdtemp()
+        filepath = os.path.join(tempdir,'best_model.pth')
+        f = open(filepath, 'wb')
+        download_file_from_google_drive(file_id, filepath)
+    else:
+        filepath = '/home/vsanil/workhorse3/physionet/best_models/best_model.pth'
+
+    # Define the model here
+    output_dim = 9
+    model = ResNet(BasicBlock, [2,2,2,2], 
+                   num_classes=output_dim, 
+                   groups=32, 
+                   width_per_group=4)
+    model.to(device)
+    
+    # Load the model
+    checkpoint = torch.load(filepath)
+    model.load_state_dict(checkpoint['model_state_dict'])
+
+    return model
+
+def classify_pytorch(data, model):
+    model.eval()
+    data = torch.from_numpy(data).float().unsqueeze(0).to(device)
+    preds = model(data)
+
+    return preds[0].cpu().detach().numpy()
+
+# Dictionary maintaing relevant framework related funcs
+lang_dict = {'pytorch':{'model':load_pytorch, 'classify':classify_pytorch}}
 
 def run_12ECG_classifier(data,header_data,classes,model):
 
@@ -17,7 +59,11 @@ def run_12ECG_classifier(data,header_data,classes,model):
     features=np.asarray(get_12ECG_features(data,header_data))
     feats_reshape = features.reshape(1,-1)
 
-    prob_score = model.predict_proba(feats_reshape)
+    preds = lang_dict[keyword]['classify'](data, model)
+    print(preds)
+    print(preds.shape)
+    exit()
+    
 
     max_prob_1 = 0
     max_prob_index = 0
@@ -32,22 +78,12 @@ def run_12ECG_classifier(data,header_data,classes,model):
             max_prob_1 = prob_1
             max_prob_index = i
     label[max_prob_index] = 1
-    # label = model.predict(feats_reshape)
-    # score = model.predict_proba(feats_reshape)
-
-    # current_label[label] = 1
-
-    # for i in range(num_classes):
-    #     current_score[i] = np.array(score[0][i])
+    
 
     return label, score
 
 def load_12ECG_model():
     # load the model from disk 
-    filename='randomforest_model.sav'
-    loaded_model = joblib.load(filename)
+    loaded_model = lang_dict[keyword]['model']()
 
     return loaded_model
-
-def load_pytorch(path):
-    filename="/share/workhorse3/vsanil/"
