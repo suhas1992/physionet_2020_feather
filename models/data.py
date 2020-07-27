290fe26f69fa1758be95ebfd1e9aa550e7f88603
Ubuntu <ubuntu@ip-172-31-55-125.us-west-2.compute.internal>
2020-07-27 18:37:57 +0000

    Added stratified multilabel splitting

--- models/data.py
+++ models/data.py
@@ -2,8 +2,9 @@ import os
 import pickle 
 import numpy as np
 import config as cfg 
+from utils.stratified_split import train_test_split
 from scipy.io import loadmat
-from sklearn.model_selection import train_test_split 
+#from sklearn.model_selection import train_test_split 
 
 import torch
 import torch.nn.utils.rnn as rnn
@@ -22,7 +23,7 @@ class ECGTrainSet(Dataset):
     def __getitem__(self, index):
         f = torch.transpose(torch.from_numpy(self.features[index]).float(), 0, 1)
 
-        l = torch.FloatTensor(self.label[index]).unsqueeze(0)
+        l = torch.LongTensor(self.label[index]).unsqueeze(0)
 
         return f, l
 
@@ -86,16 +87,16 @@ def get_loader(loader_type, val_exists=True, feature_dict=None):
     loader_set = None
     if val_exists:
         if loader_type == "train":
-            train_idx, _ = train_test_split(list(range(len(dataset))), 
+            train_idx, _ = train_test_split(list(range(len(dataset))), dataset=dataset,
                                             test_size=cfg.VAL_SPLIT+cfg.TEST_SPLIT,
                                             shuffle=True, random_state=cfg.RANDOM_SEED)
             loader_set = Subset(dataset, train_idx)
         elif loader_type == "val" or loader_type == "test":
-            _, temp_idx = train_test_split(list(range(len(dataset))), 
+            _, temp_idx = train_test_split(list(range(len(dataset))), dataset=dataset,
                                         test_size=cfg.VAL_SPLIT+cfg.TEST_SPLIT,
                                         shuffle=True, random_state=cfg.RANDOM_SEED)
             temp_set = Subset(dataset, temp_idx)
-            val_idx, test_idx = train_test_split(list(range(len(temp_set))),
+            val_idx, test_idx = train_test_split(list(range(len(temp_set))), dataset=temp_set,
                                                 test_size= 1-(cfg.TEST_SPLIT/(cfg.VAL_SPLIT + cfg.TEST_SPLIT)),
                                                 shuffle=True, random_state=cfg.RANDOM_SEED)
             if loader_type == "val":
---
+++ models/utils/stratified_split.py
@@ -0,0 +1,30 @@
+import numpy as np
+import random
+
+def train_test_split(idxs, dataset, test_size, shuffle, random_state):
+    count_dict = {}
+    data_len = len(dataset)
+    percent = test_size/data_len
+    for i in idxs:
+        tup_l = repr(dataset[i][1].numpy().tolist())
+        if tup_l not in count_dict:
+            count_dict[tup_l] = {"idx":[i],
+                                 "count":1}
+        else:
+            count_dict[tup_l]["idx"].append(i)
+            count_dict[tup_l]["count"] += 1
+
+    train_idxs = []
+    test_idxs = []
+    for key, values in count_dict.items():
+        train_split = data_len - int(values["count"]*percent)
+        train_idxs.extend(values["idx"][:train_split])
+        test_idxs.extend(values["idx"][train_split:])
+
+    
+    if shuffle:
+        random.seed(random_state)
+        random.shuffle(train_idxs)
+        random.shuffle(test_idxs)
+
+    return train_idxs, test_idxs
