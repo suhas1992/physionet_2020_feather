062c55aa4abacc703551f4c7d6eaa63b267f1c50
Ubuntu <ubuntu@ip-172-31-55-125.us-west-2.compute.internal>
2020-08-14 02:20:32 +0000

    Added modifications for class stratified training

--- models/utils/get_class_dict.py
+++ models/utils/get_class_dict.py
@@ -1,6 +1,7 @@
 import os
 import csv  
 import psutil
+import random
 import numpy as np
 from scipy.io import loadmat
 
@@ -12,6 +13,60 @@ CLASS_DICT = {1:["270492004", "164889003", "164890007", "426627000", "10370003",
               6:["39732003","47665007"],
               7:["698252002"],
               8:["427172004","63593006","17338001"]}
+
+RESAMP = 0.25
+
+def extract_individual_group_data(files, snomed_code, diagnosis, group_num):
+    # Extract all positive data samples
+    feature_dict = {'features':[], 'labels':[]}
+    p_count = 0
+    for idx, f in enumerate(files):
+        if f.endswith('.mat'):
+            #print(count, f, idx)
+            if count % 10000 == 0:
+                print("RAM used: ", psutil.virtual_memory().percent, "Files done: ", count)
+            data, header = load_challenge_data(f)
+            label = header[-4].replace("#Dx: ","").replace("\n","").split(',')
+            add = False 
+            for lbl in label:
+                if lbl == snomed_code:
+                    l = [1,0]
+                    add = True
+                    p_count += 1
+            if add:
+                feature_dict['features'].append(data)
+                feature_dict['labels'].append(l) 
+
+    # Extract all negative data samples
+    in_group, out_group = p_count-int(p_count*RESAMP), int(p_count*RESAMP)
+    random.shuffle(files)
+    for idx, f in enumerate(files):
+        if in_group == 0 and out_group == 0:
+            break
+        if f.endswith('.mat'):
+            if count % 10000 == 0:
+                print("RAM used: ", psutil.virtual_memory().percent, "Files done: ", count)
+            data, header = load_challenge_data(f)
+            label = header[-4].replace("#Dx: ","").replace("\n","").split(',')
+            add = False
+            if snomed_code not in label:
+                for lbl in label:
+                    if lbl in CLASS_DICT[group_num]:
+                        if in_group != 0:
+                            in_group -= 1
+                            l = [0,1]
+                            add = True
+                    else:
+                        if out_group != 0:
+                            out_group -= 1
+                            l = [0,1]
+                            add = True
+            if add:
+                feature_dict['features'].append(data)
+                feature_dict['labels'].append(l) 
+
+    print("Features for diagnosis {} extracted".format(diagnosis))
+    return feature_dict
 
 def get_class_group(diagnoses):
     """
